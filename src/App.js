ad0eecf73caa652fe5f21a7c04171f006c51a5f0
MavunoX <mavunoxafrica@gmail.com>
2024-01-22 17:07:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Routes, Route, HashRouter } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Gallery from "./pages/gallery/Gallery";
+import Plans from "./pages/plans/Plans";
+import Trainers from "./pages/trainers/Trainers";
+import NotFound from "./pages/notFound/NotFound";
+import Footer from "./components/Footer";
+
+function App() {
+	return (
+		<HashRouter>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/gallery" element={<Gallery />} />
+				<Route path="/plans" element={<Plans />} />
+				<Route path="/trainers" element={<Trainers />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			<Footer />
+		</HashRouter>
+	);
+}
+
+export default App;
---
+++ src/pages/gallery/Gallery.js
@@ -0,0 +1,31 @@
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_3.jpg";
+import "./Gallery.css";
+
+const Gallery = () => {
+	// const galleryLength = 15
+	let images = [];
+	for (let i = 1; i <= 15; i++) {
+		images.push(require(`../../images/gallery${i}.jpg`));
+	}
+
+	return (
+		<>
+			<Header image={HeaderImage} title="Gallery">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis optio
+				maxime temporibus sunt laborum deleniti.
+			</Header>
+			<section className="gallery">
+				<div className="container gallery__container">
+					{images.map((image, index) => (
+						<article key={index}>
+							<img src={image} alt={`Gallery ${index + 1}`} />
+						</article>
+					))}
+				</div>
+			</section>
+		</>
+	);
+};
+
+export default Gallery;
